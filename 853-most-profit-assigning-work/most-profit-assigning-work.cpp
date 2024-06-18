61138e68c97662a2b45187bcad44e515b52a1376
Arun Parihar <59966935+nikarun@users.noreply.github.com>
2024-06-18 10:02:59 +0530

    Time: 52 ms (54.38%) | Memory: 41 MB (72.17%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -8,17 +8,20 @@ public:
             vec.push_back({difficulty[i],profit[i]});
         }
         sort(vec.begin(),vec.end());
+        for(int i=1;i<n;++i){
+            vec[i].second=max(vec[i].second,vec[i-1].second);
+        }
         for(int i=0;i<worker.size();++i){
             int max_profit=0;
-            for(int j=0;j<vec.size();++j){
-                if(vec[j].first<=worker[i]){
-                    max_profit=max(max_profit,vec[j].second);
+            int low=0,high=n-1;
+            while(low<=high){
+                int mid=(low+high)/2;
+                if(vec[mid].first<=worker[i]) {
+                    low=mid+1;
                 }
-                else{
-                    break;
-                }
+                else high=mid-1;
             }
-            res+=max_profit;
+            if(high>=0) res+=vec[high].second;
         }
         return res;
     }
