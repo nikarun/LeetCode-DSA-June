6579f0416b1a085dd7921db0d2cf47de8a632455
Arun Parihar <59966935+nikarun@users.noreply.github.com>
2024-06-25 09:19:13 +0530

    Time: 2 ms (49.63%) | Memory: 10.2 MB (12.64%) - LeetSync

--- 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -11,21 +11,18 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode *root, vector<TreeNode*>&inorder){
+    void dfs(TreeNode *root, int& cnt){
         if(root==NULL) return;
-        dfs(root->left,inorder);
-        inorder.push_back(root);
-        dfs(root->right,inorder);
+        dfs(root->right,cnt);
+        root->val+=cnt;
+        cnt=root->val;
+        dfs(root->left,cnt);
     }
     TreeNode* bstToGst(TreeNode* root) {
         if(root==NULL) return NULL;
         if(root->left == NULL && root->right==NULL) return root;
-        vector<TreeNode*>inorder;
-        dfs(root,inorder);
-        int n=inorder.size();
-        for(int i=n-2;i>=0;--i){
-            inorder[i]->val=inorder[i]->val+inorder[i+1]->val;
-        }
+        int cnt=0;
+        dfs(root,cnt);
         return root;
     }
 };
