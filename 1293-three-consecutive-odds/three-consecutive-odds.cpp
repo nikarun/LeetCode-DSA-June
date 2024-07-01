b087beb4eea0d51de0201f2fa008cfe3f9946ca5
Arun Parihar <59966935+nikarun@users.noreply.github.com>
2024-07-01 09:14:21 +0530

    Time: 3 ms (60.88%) | Memory: 10.3 MB (99.07%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -4,13 +4,12 @@ public:
         int odds=0;
         int n=arr.size();
         for(int i=0;i<n;++i){
+            if(odds==3) return true;
             if(arr[i]%2!=0) ++odds;
             else{
-                if(odds==3) return true;
                 odds=0;
             }
-            if(odds==3) return true;
         }
-        return false;
+        return odds>=3?true:false;
     }
 };
