8ddbc4809ce952ccda7f0ef80df51b02a1e84a0a
Arun Parihar <59966935+nikarun@users.noreply.github.com>
2024-08-04 15:03:02 +0530

    Time: 130 ms (9.66%) | Memory: 27.3 MB (19.10%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -3,22 +3,23 @@ public:
     int mod=1e9+7;
     int rangeSum(vector<int>& nums, int n, int left, int right) {
         if(n==0) return 0;
-        vector<int>subArraySum;
+        priority_queue<int,vector<int>,greater<int>>pq;
         for(int i=0;i<n;++i){
             int sum=nums[i];
-            subArraySum.push_back(sum);
+            pq.push(sum);
             for(int j=i+1;j<n;++j){
                 sum+=nums[j];
-                subArraySum.push_back(sum);
+                pq.push(sum);
             }
         }
-        sort(subArraySum.begin(),subArraySum.end());
-        for(int j=0;j<subArraySum.size();++j){
-            cout<<subArraySum[j]<<"  ";
-        }
+
         long long res=0;
-        for(int i=left;i<=right;++i){
-            res+=subArraySum[i-1]%mod;
+        for(int i=1;i<=right;++i){
+            int top=pq.top();
+            pq.pop();
+            if(i>=left){
+                res+=top%mod;
+            }
         }
         return res%mod;
 
@@ -29,3 +30,4 @@ public:
 // gen all subarrays -->n2
 // sort them 
 // fetch sum from le to re
+
