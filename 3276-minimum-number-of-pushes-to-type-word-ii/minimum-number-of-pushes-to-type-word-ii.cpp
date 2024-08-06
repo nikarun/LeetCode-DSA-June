fb9460d9552d3f89dcb9a1c41639c44d7d99b1ce
Arun Parihar <59966935+nikarun@users.noreply.github.com>
2024-08-06 19:35:57 +0530

    Time: 66 ms (87.01%) | Memory: 27.7 MB (14.43%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-    static bool comp(const pair<char,int>&a, const pair<char,int>&b){
-        return a.second>b.second;
-    }
     int minimumPushes(string word) {
         int n=word.size();
-        unordered_map<char,int>mp;
+        vector<int>freq(26,0);
         for(int i=0;i<word.size();++i){
-            mp[word[i]]++;
+            freq[word[i]-'a']++;
         }
-        vector<pair<char,int>>vec;
-        for(auto it=mp.begin();it!=mp.end();++it){
-            vec.push_back({it->first,it->second});
-        }
-        sort(vec.begin(),vec.end(),comp);
+        sort(freq.rbegin(),freq.rend());
         int res=0;
         //place first 9 most frequent characters in first postion
         //then next 9 on second position
         //next 9 on rd postion & so on
         int count=1;//unique characters
         int pos=1; //position
-        for(int i=0;i<vec.size();++i){
+        for(int i=0;i<26;++i){
+            if(freq[i]==0) break;
             if(count>8){
                 pos+=1;
                 count=1;
             }
-            res+=(vec[i].second*pos);
-            cout<<" vec "<<vec[i].second<<" "<<" res "<<res<<" pos "<<pos<<" count "<<count<<endl;
+            res+=(freq[i]*pos);
             ++count;
         }
         return res;
