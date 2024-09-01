bd675f3150c39461044c85ccaa93402e7a892ed7
Arun Parihar <59966935+nikarun@users.noreply.github.com>
2024-09-01 12:20:27 +0530

    Time: 66 ms (96.04%) | Memory: 88.1 MB (73.42%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
@@ -8,9 +8,6 @@ public:
         for(int i=0;i<m;++i){
             for(int j=0;j<n;++j){
                 res[i][j]=original[k++];
-                if(k>=original.size()){
-                    break;
-                }
             }
         }
      return res;   
