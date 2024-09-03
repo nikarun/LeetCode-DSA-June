622a91661679ef8f202c6c8d4fdc5ffed76d2eb0
Arun Parihar <59966935+nikarun@users.noreply.github.com>
2024-09-03 09:46:44 +0530

    Time: 3 ms (51.28%) | Memory: 9.9 MB (50.85%) - LeetSync

---
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int getLucky(string s, int k) {
+     int n=s.size();
+     string numericString="";
+     for(int i=0;i<n;++i){
+        int baseValue=int(s[i])-96;
+        numericString+=to_string(baseValue);
+     }  
+     cout<<numericString<<endl; 
+     while(k>0){
+        int res=0;
+        n=numericString.size();
+        for(int i=0;i<n;++i){
+            res+=int(numericString[i])-'0';
+        }
+        // cout<<res<<endl;
+        numericString=to_string(res);
+        --k;
+     }
+     return stoi(numericString);
+    }
+};
