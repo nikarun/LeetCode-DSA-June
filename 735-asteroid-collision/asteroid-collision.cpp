eea5882563fc3c844b6a97667c5d61942ca2f148
Arun Parihar <59966935+nikarun@users.noreply.github.com>
2024-10-05 13:31:34 +0530

    Time: 7 ms (84.93%) | Memory: 21 MB (17.80%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-     int n=asteroids.size();
-     if(n==0){
-        return {};
-     }   
-     stack<int>st;
-     for(int i=0;i<n;++i){
-        int unit=asteroids[i];
-        if(unit>0 || st.empty()){
-            st.push(unit);
-        }
-        else{
-                while(!st.empty() && st.top()>0 && st.top()<abs(unit)){
-                    st.pop();
-                }
-                if(!st.empty() && st.top()==abs(unit)) st.pop();
+        int n=asteroids.size();
+        if(n==0) return {};
+        vector<int>res;
+        stack<int>st;
+        for(int i=0;i<n;++i){
+            int curr_ast = asteroids[i];
+            if(curr_ast<0){
+                if(st.empty()) st.push(curr_ast);
                 else{
-                    if(st.empty() || st.top()<0) st.push(unit);
+                    while(!st.empty() && st.top()>0 && st.top()<abs(curr_ast)){
+                        st.pop();
+                    }
+                    if(st.empty() || st.top()<0) st.push(curr_ast);
+                    if(st.top() == abs(curr_ast) && st.top()>0) st.pop();
+                    
                 }
+            }
+            else{
+                st.push(curr_ast);
+            }
         }
-     }
-     vector<int>res;
-     while(!st.empty()){
-        res.push_back(st.top());
-        st.pop();
-     }
-     reverse(res.begin(),res.end());
-     return res;
+        while(!st.empty()){
+            res.push_back(st.top());
+            st.pop();
+        }
+        reverse(res.begin(),res.end());
+        return res;
     }
 };
