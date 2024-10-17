f3d51f6260b846060ece98e7665d45857831199b
Arun Parihar <59966935+nikarun@users.noreply.github.com>
2024-10-17 22:33:31 +0530

    Time: 94 ms (45.71%) | Memory: 13.3 MB (50.71%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -4,13 +4,13 @@ public:
         int n=nums.size();
         if(n==0) return 0;
         vector<int>dp(n,1);
-        int max_len=0;
+        int max_len=1;
         for(int i=0;i<n;++i){
             for(int j=0;j<i;++j){
                 if(nums[i]>nums[j]) dp[i]=max(dp[i],dp[j]+1);
                 max_len=max(max_len,dp[i]);
             }
         }
-        return max(max_len,dp[n-1]);
+        return max_len;
     }
 };
