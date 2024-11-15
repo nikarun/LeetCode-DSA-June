6170bef4648a7af2f5d7f04565df0234e310086f
Arun Parihar <59966935+nikarun@users.noreply.github.com>
2024-11-15 12:53:40 +0530

    Time: 0 ms (100.00%) | Memory: 26.4 MB (52.94%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -14,17 +14,17 @@ public:
     int maxSum=INT_MIN;
     int dfs(TreeNode *root){
         if(root==NULL) return 0;
-        // if(root->left==NULL && root->right==NULL) return root->val;
         int leftSum=dfs(root->left);
         int rightSum=dfs(root->right);
         if(leftSum<0) leftSum=0;
         if(rightSum<0) rightSum=0;
         maxSum=max(maxSum,leftSum+rightSum+root->val);
-        return max(leftSum+root->val,rightSum+root->val);
-    }
+        return max(leftSum,rightSum)+root->val;
+    } 
     int maxPathSum(TreeNode* root) {
         if(root==NULL) return 0;
-        int sum=dfs(root);
-        return max(maxSum,sum);
+        int sum=0;
+        dfs(root);
+        return maxSum;
     }
 };
