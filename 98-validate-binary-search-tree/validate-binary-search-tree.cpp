6491fe41cb9fe4c71060b3d0a6eaee52084863bf
Arun Parihar <59966935+nikarun@users.noreply.github.com>
2024-11-18 10:50:51 +0530

    Time: 0 ms (100.00%) | Memory: 20.2 MB (95.24%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -11,18 +11,19 @@
  */
 class Solution {
 public:
-    bool dfs(TreeNode *root, int *upper, int *lower){
-        if(!root) return true;
-        if(upper){
-            if(root->val>=*upper) return false;
-        }
+    bool isValid(TreeNode *root, int *lower, int *upper){
+        if(root==NULL) return true;
         if(lower){
             if(root->val<=*lower) return false;
         }
-        return dfs(root->left,&(root->val),lower) && dfs(root->right,upper,&(root->val));
+        if(upper){
+            if(root->val>=*upper) return false;
+        }
+        // if(root->val<=lower || root->val>=upper) return false;
+        return isValid(root->left,lower,&(root->val)) && isValid(root->right,&(root->val),upper);
     }
     bool isValidBST(TreeNode* root) {
         if(root==NULL) return true;
-        return dfs(root, NULL, NULL);
+        return isValid(root,NULL,NULL);
     }
 };
