a37beb437fc03b21ab2102940146a027c952b3b9
Arun Parihar <59966935+nikarun@users.noreply.github.com>
2024-11-22 08:22:45 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (30.38%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int sr, int sc, int color, int currColor, int m, int n){
-        if(sr>m-1 || sc>n-1 || sr<0 || sc<0 || image[sr][sc]!=currColor || image[sr][sc]==color) return;
+    void dfs(vector<vector<int>>& image, int sr, int sc, int color, int currColor, int m, int n, vector<vector<bool>>&vis){
+        if(sr>m-1 || sc>n-1 || sr<0 || sc<0 || image[sr][sc]!=currColor || image[sr][sc]==color || vis[sr][sc]) return;
         image[sr][sc]=color;
-        dfs(image,sr+1,sc,color,currColor,m,n);
-        dfs(image,sr-1,sc,color,currColor,m,n);
-        dfs(image,sr,sc+1,color,currColor,m,n);
-        dfs(image,sr,sc-1,color,currColor,m,n);
+        vis[sr][sc]=true;
+        dfs(image,sr+1,sc,color,currColor,m,n,vis);
+        dfs(image,sr-1,sc,color,currColor,m,n,vis);
+        dfs(image,sr,sc+1,color,currColor,m,n,vis);
+        dfs(image,sr,sc-1,color,currColor,m,n,vis);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int m=image.size();
         int n=image[0].size();
         if(m==0 && n==0) return {{}};
         int currColor=image[sr][sc];
-        dfs(image,sr,sc,color,currColor,m,n);
+        vector<vector<bool>>vis(m,vector<bool>(n,false));
+        dfs(image,sr,sc,color,currColor,m,n,vis);
         return image;
     }
 };
