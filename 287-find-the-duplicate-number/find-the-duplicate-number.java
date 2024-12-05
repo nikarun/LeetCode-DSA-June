2eeb916b48154fbab8e0bf838af6369850730190
Arun Parihar <59966935+nikarun@users.noreply.github.com>
2024-12-05 08:24:50 +0530

    Time: 4 ms (87.48%) | Memory: 58.4 MB (38.74%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,11 +1,17 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int n=nums.length;
-        HashMap<Integer,Integer>mp=new HashMap<Integer,Integer>();
-        for(int i=0;i<n;++i){
-            if(mp.containsKey(nums[i])) return nums[i];
-            mp.put(nums[i],i);
+        //Linked list cycle detection
+        int slow=0,fast=0;
+        while(true){
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+            if(slow==fast) break;
         }
-        return -1;
+        int check=0;
+        while(true){
+            slow=nums[slow];
+            check=nums[check];
+            if(slow==check) return check;
+        }
     }
 }
