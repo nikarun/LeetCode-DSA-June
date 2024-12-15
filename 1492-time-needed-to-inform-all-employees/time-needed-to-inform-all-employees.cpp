29c2786117e9b46d9adfb2e0affc2e003965d5ba
Arun Parihar <59966935+nikarun@users.noreply.github.com>
2024-12-16 00:30:52 +0530

    Time: 105 ms (67.30%) | Memory: 126.2 MB (69.35%) - LeetSync

--- 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.cpp
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     int maxInformTime=0;
-    int itr=0;
     void dfs(int root, vector<int>& informTime, vector<vector<int>> &adj, int time){
-        ++itr;
-        cout<<itr<<endl;
         if(adj[root].size()==0) maxInformTime=max(maxInformTime, time);
         for(int i=0;i<adj[root].size();++i){
             dfs(adj[root][i],informTime,adj, time+informTime[root]);
