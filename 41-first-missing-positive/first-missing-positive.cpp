903420b724291b18df91b47322f03ef0347c0f5d
Arun Parihar <59966935+nikarun@users.noreply.github.com>
2025-02-26 11:32:57 +0530

    Time: 0 ms (100.00%) | Memory: 54.9 MB (93.41%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int n=nums.size();
-        if(n==1){
-            if(nums[0]!=1) return 1;
-            else return 2;
+        if(n==0) return 0;
+        //bring elements to their positions.
+        for(int i=0;i<n;++i){
+            while(nums[i]>0 && nums[i]<=n && nums[i]!=nums[nums[i]-1]){
+                swap(nums[i],nums[nums[i]-1]);
+            }
         }
-        unordered_map<int,int>mp;
-        for(int num:nums){
-            mp[num]++;
+        for(int i=0;i<n;++i){
+            if(nums[i]!=i+1) return i+1;
         }
-
-        for(int i=1;i<=n+1;++i){
-            if(mp[i]==0) return i;
-        }
-        return -1;
-
+        return n+1;
     }
 };
-
-// mp->freq,
-// 1 ->len(n)
+// n=6
+// [-1,4,2,1,9,10]
+// // [-1,1,2,4,9,10]
+// // [-1,2,1,4,9,10]
