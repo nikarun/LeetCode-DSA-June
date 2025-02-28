d6c0e5cde51c6f405704b85d69c4560f99992a15
Arun Parihar <59966935+nikarun@users.noreply.github.com>
2025-02-28 09:13:25 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (58.16%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
-    int ones(int num){
-        int cnt=0;
-        while(num>0){
-        if(num%2!=0) ++cnt;
-        num/=2; 
-        }
-        return cnt;
-    }
-
     vector<int> countBits(int n) {
-        vector<int>res;
-        for(int i=0;i<=n;++i){
-            res.push_back(ones(i));
+        vector<int>res(n+1,0);
+        for(int i=1;i<=n;++i){
+            if(i%2==0) res[i]=res[i/2];
+            else res[i]=res[i-1]+1;
         }
         return res;
     }
 };
+
+
+
+// 0->0
+// 1-->1
+// 2-->010
+// 3-->11
+// 4-->100
+// 5-->101
+// 6-->110
+// 7-->111
+// 8-->1000
+// 9-->1001
+// 10-->1010
+// 11-->1011
+// 12-->1100
+// 13 -->1101
+// 14 ->1110
+// 15-->1111
+
+
+// for odd prev+1.
+// for event ->half
